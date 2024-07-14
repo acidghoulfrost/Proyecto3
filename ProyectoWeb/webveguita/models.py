2a00bbbdf67af7e79cb0043d14778361518d30fb
Cristobal <71562806+acidghoulfrost@users.noreply.github.com>
2024-07-14 17:33:02 -0400

    a

--- ProyectoWeb/webveguita/models.py
+++ ProyectoWeb/webveguita/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class Cliente(models.Model):
-    rut = models.CharField(primary_key=True, max_length=10)
-    nombre = models.CharField(max_length=20)
-    apellido = models.CharField(max_length=20)
-    telefono = models.CharField(max_length=45)
-
+class Cajas(models.Model):
+    id_caja=models.CharField(primary_key=True, max_length=8, verbose_name="Codigo Caja")
+    tipo_caja= models.CharField(max_length=50, verbose_name="Tipo Caja")
+    peso_kg=models.CharField(max_length=50, verbose_name="Peso KG")
+    dimension=models.CharField(max_length=50, verbose_name= "Dimension Caja")
+    empresa_envio=models.ForeignKey(Categoria, on_delete=models.CASCADE, verbose_name="Categoria")
+    
     def __str__(self):
-        return self.nombre
+        return self.id_caja
     
 class Cajas(models.Model):
     cod_caja = models.CharField(primary_key=True, max_length=5)
@@ -29,6 +30,9 @@ class Boleta(models.Model):
     fecha_recepcion = models.DateField()
     fecha_entrega = models.DateField(blank=True, null=True)
     cajas = models.ManyToManyField(Cajas)
+
+class Detalle_boleta(models.Model):
+    cod_boleta = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return f'Boleta {self.id_boleta} - {self.nombre_cliente}'
